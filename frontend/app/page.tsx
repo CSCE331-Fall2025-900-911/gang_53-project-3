98587948cb92d99d849890745f45fec4ab5770fb
Jayden <jkohhokj@gmail.com>
2025-10-29 19:21:02 -0500

    push hello world

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,6 +13,9 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="text-9xl">
+            HELLO WORLD
+          </h1>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
            WELCOME TO BOBALICIOUS!!!
           </h1>
